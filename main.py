ecb478599239584118e147b4f39c91d839f3801d
vijayshankarrealdeal <vijaygaya056@gmail.com>
2022-08-14 22:35:41 +0530

    git push

--- main.py
+++ main.py
@@ -6,45 +6,23 @@ from sqlalchemy import create_engine
 app = FastAPI()
 
 
-key = "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"
+key = "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"
 gptCall = GPTQuery(key)
 db = create_engine('postgresql://postgres:1234567890@localhost:5432/slab')
 
-@app.get('/{user_query}')
+@app.get('/query/{user_query}')
 def index(user_query:str):
+    if 'year' in user_query:
+        user_query = user_query.replace('year','years')
     statement = gptCall.make_sql_statement(user_query)
     result_set = db.execute(statement)
-    print(statement)
     data = [row._asdict() for row in result_set]
     return data
 
-
-    
-
-
-@app.get('/adt')
+@app.get('/app/top_rating')
 def company():
-    return_data = []
-    for d in data:
-        # print(d)
-        return_data.append([d['Supplier Name'],d['Average Delivery Time']])
+    statement = 'SELECT * FROM stable where rating > 90'
+    result_set = db.execute(statement)
+    data = [row._asdict() for row in result_set]
+    return data
     
-    return return_data
-
-@app.get('/companies')
-def company():
-    return_data = []
-    for d in data:
-        # print(d)
-        return_data.append([d['Supplier Name'], d['Function']])
-    
-    return return_data
-
-@app.get('/resources')
-def company():
-    return_data = []
-    for d in data:
-        # print(d)
-        return_data.append([d['Supplier Name'], d['Resources']])
-    
-    return return_data
